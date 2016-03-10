6626b309e781d31f11c8b77806cf3441e3d14572
Matthaus Owens <matthaus@puppetlabs.com>
2016-03-10 13:21:20 -0800

    Remove tests

--- test/puppetlabs/missing_objects_as_a_service_core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns puppetlabs.missing-objects-as-a-service-core-test
-  (:require [clojure.test :refer :all]
-            [puppetlabs.missing-objects-as-a-service-core :refer :all]))
-
-(deftest hello-test
-  (testing "says hello to caller"
-    (is (= "Hello, foo!" (hello "foo")))))
